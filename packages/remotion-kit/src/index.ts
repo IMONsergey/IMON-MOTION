3d237e40228616a9fe02a4ac6303ef6b9b339615
Sergei I'MON <jabba.person@gmail.com>
2026-08-12 08:33:08 +0300

    fix(remotion): use extensionless runtime exports

--- packages/remotion-kit/src/index.ts
+++ packages/remotion-kit/src/index.ts
@@ -1,3 +1,3 @@
-export * from './choreography.js';
-export * from './primitives.js';
-export * from './scenes.js';
+export * from './choreography';
+export * from './primitives';
+export * from './scenes';
