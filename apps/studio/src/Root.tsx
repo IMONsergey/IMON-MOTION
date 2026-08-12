e188a07e04c4a4c4f405e18ca812d6b1ab0d941a
Sergei I'MON <jabba.person@gmail.com>
2026-08-12 08:20:04 +0300

    feat(studio): register IMON system preview composition

---
+++ apps/studio/src/Root.tsx
@@ -0,0 +1,15 @@
+import { Composition } from 'remotion';
+import { SystemPreview } from './SystemPreview.js';
+
+export const RemotionRoot = () => (
+  <>
+    <Composition
+      id="IMONSystemPreview"
+      component={SystemPreview}
+      durationInFrames={360}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  </>
+);
